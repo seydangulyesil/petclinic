fe6671ca83caf1f8e29d2ae298bbb735ed22058f
seydangulyesil <seydan.gulyesil@hotmail.com>
2024-09-27 20:19:43 +0000

    added qa automation pipeline for dev

---
+++ ansible/scripts/run_selenium_jobs.sh
@@ -0,0 +1,2 @@
+ATH="$PATH:/usr/local/bin"
+ansible-playbook -vvv --connection=local --inventory 127.0.0.1, --extra-vars "workspace=${WORKSPACE} master_public_ip=${MASTER_PUBLIC_IP}" ./ansible/playbooks/pb_run_selenium_jobs.yaml
